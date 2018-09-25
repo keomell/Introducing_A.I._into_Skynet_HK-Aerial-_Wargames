7da7d6855b1afbfbb67891910b2d8d61b11ad83a
Ke'Ondrae Mell <ondrae100@gmail.com>
2018-09-25 13:43:51 -0700

    Commit

---
+++ cmake-build-debug/CMakeFiles/Introducing_A_I__into_Skynet_HK_Aerial__Wargames.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Introducing_A_I__into_Skynet_HK_Aerial__Wargames.dir/main.cpp.o"
+  "Introducing_A_I__into_Skynet_HK_Aerial__Wargames.pdb"
+  "Introducing_A_I__into_Skynet_HK_Aerial__Wargames"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Introducing_A_I__into_Skynet_HK_Aerial__Wargames.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Introducing_A_I__into_Skynet_HK_Aerial__Wargames.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Introducing_A_I__into_Skynet_HK_Aerial__Wargames.
+# This may be replaced when dependencies are built.
